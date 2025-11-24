840cd413f495d9abab5d890d55ac03a547e931df
s1lverfoot <60921307+S1lverFoot@users.noreply.github.com>
2025-11-24 14:51:36 +0200

    Add files via upload

---
+++ lr5.js
@@ -0,0 +1,28 @@
+function seq(f) {
+    const funcs = [f];
+
+  function chain(arg) {
+    if (typeof arg === 'function') {
+      funcs.push(arg);
+      return chain;
+    } else if (typeof arg === 'number') {
+      return funcs.reduceRight((result, fn) => fn(result), arg);
+    }
+  }
+
+  return chain;
+}
+
+const res1 = seq(x => x + 7)
+                (x => x * 2)(5);
+console.log(res1); // 17
+
+const res2 = seq(x => x * 2)
+                (x => x + 7)(5);
+console.log(res2); // 24
+
+const res3 = seq(x => x + 1)
+                (x => x * 2)
+                (x => x / 3)
+                (x => x - 4)(7);
+console.log(res3); // 3
